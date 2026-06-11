2eebcf1abe1a7a672f381c79a788800904f2a172
Ravitheja <gollaravitheja00@gmail.com>
2026-06-11 12:00:29 +0530

    env config for frontend

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -8,7 +8,7 @@ export const AuthProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const API_URL = 'http://localhost:5000/api';
+  const API_URL = import.meta.env.VITE_API_URL
 
   useEffect(() => {
     const loadUser = async () => {
